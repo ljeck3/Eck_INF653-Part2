6ed15e879f5a36e9561b55d75f2e1672b6319d64
lj-1003 <levijustin24@gmail.com>
2026-05-13 12:40:29 -0500

    Added jwt middleware.

--- final/controller/authController.js
+++ final/controller/authController.js
@@ -1,5 +1,6 @@
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
  
 const handleLogin = async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,10 +11,16 @@ const handleLogin = async (req, res) => {
  
   const foundUser = await User.findOne({ email: email }).exec();
   if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
- 
+  
   const match = await bcrypt.compare(password, foundUser.password);
   if (match) {
-    res.json({ success: `${email} is logged in.` });
+    const accessToken = jwt.sign(
+      { email: foundUser.email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {expiresIn: '15m'}
+      );
+
+    res.json({ accessToken });
   } else {
     res.sendStatus(401);
   }
